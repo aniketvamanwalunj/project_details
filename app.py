b5a4f3351ef436d8b926796fb0a60048bc4d9bb3
Aniket Walunj <142065953+aniketvamanwalunj@users.noreply.github.com>
2024-11-17 21:33:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Directly display the project details page on the home route
+@app.route('/')
+def project_details():
+    return render_template('project_details.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
